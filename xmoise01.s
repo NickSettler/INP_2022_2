4e178f0f9b32c946a8c465cc62403989b35cf9fb
NickSettler <moiseevnikita14@gmail.com>
2022-10-13 01:38:38 +0200

    (perf) refactors code to decrease CPI

--- xmoise01.s
+++ xmoise01.s
@@ -14,10 +14,16 @@ params_sys5:    .space  8   ; misto pro ulozeni adresy pocatku
 
 ; CODE SEGMENT
                 .text
-
-                ; ZDE NAHRADTE KOD VASIM RESENIM
                 ; xmoise01-r2-r16-r15-r23-r0-r4
+                ; Raw stalls: 84 -> 83 -> 77 -> 65 -> 56 -> 47
 main:
+                ; Intro
+                ; Some code may look strange. For example, computing
+                ; in the end of the while loop, or missing computings
+                ; in the beginning of the loop. Such instructions location
+                ; are made to achieve the best performance and lower CPI.
+                ;
+                ; Registers:
                 ; r0    stores constant 0
                 ; r2    stores result of comparing instructions (slt / sltu / slti / sltui).
                 ; r4    stores pointer to login string or pointer to it.
@@ -25,62 +31,60 @@ main:
                 ;       used to check if character is letter or number.
                 ; r16   stores index of current character in login string
                 ; r23   stores 'm' and 'o' symbols to add or substract them from login character
-                daddi   r16, r16, 0
-                lbu     r4, 0(r16)   ; read first login character
-
+                daddi   r23, r0, 0x0F
                 daddi   r15, r0, 0x0D
-                sb      r15, 16(r0)  ; save character 'm' to memory
-                daddi   r15, r0, 0x0F
-                sb      r15, 17(r0)  ; save character 'o' to memory
+                lbu     r4, 0(r0)   ; read first login character
 
-                xor     r15, r15, r15
+                ; Set r15 to 'm' and r23 'o' symbols
+                sb      r23, 17(r0)
+                daddi   r23, r0, 0x02
+                sb      r15, 16(r0)
+                daddi   r15, r0, 0x61
 
                 ; begin while(*login[r4] < 97)
                 start_while:
-                daddi   r15, r0, 0x61
                 sltu    r2, r4, r15  ; *login[r4] < 97
+                mfhi    r15
                 bnez    r2, end_while
                 ; begin loop instructions
                     ; begin if(r16 % 2 == 0) symbol_plus(); else symbol_minus();
-                    daddi   r23, r0, 0x02
-                    div     r16, r23
-                    mfhi    r15
                     beqz    r15, symbol_plus
                     bnez    r15, symbol_minus
                     b       end_if
 
                     symbol_plus:
-                        xor     r15, r15, r15
-                        ; *login[r4] += 'm';
                         lb      r23, 16(r0)
+                        daddi   r15, r0, 0x7A
+                        ; *login[r4] += 'm';
                         add     r4, r4, r23
                         ; if(*login[r4] > 122) *login[r4] -= 26;
-                        daddi   r15, r0, 0x7A
                         sltu    r2, r15, r4
+                        daddi   r15, r0, 0x1A
                         bnez    r2, symbol_minus_26
                         b       end_if
                         symbol_minus_26:
-                            daddi   r15, r0, 0x1A
                             sub     r4, r4, r15
                         b       end_if
                     symbol_minus:
-                        xor     r15, r15, r15
-                        ; *login[r4] -= 'o';
                         lb      r23, 17(r0)
+                        daddi   r15, r0, 0x61
+                        ; *login[r4] -= 'o';
                         sub     r4, r4, r23
                         ; if(*login[r4] < 97) *login[r4] += 26;
-                        daddi   r15, r0, 0x61
                         sltu    r2, r4, r15
+                        daddi   r15, r0, 0x1A
                         bnez    r2, symbol_plus_26
                         b       end_if
                         symbol_plus_26:
-                            daddi   r15, r0, 0x1A
                             add     r4, r4, r15
                         b       end_if
                     end_if:
+                    daddi   r15, r0, 0x61
+                    daddi   r23, r0, 0x02
 
                     sb      r4, 0(r16)
                     daddi   r16, r16, 1   ; login[r4] = login[r4] + 8
+                    div     r16, r23
 
                     lbu     r4, 0(r16)    ; reading next character
                     b start_while
